536dbe8a3901cd7916a71aa7f2eb6ee4d1448ddd
Natalia Mamunina <mamunina-nv@roxiemobile.com>
2022-10-04 17:07:04 +0300

    NSFPIO-1141 'CocoaPods' remove from project.

--- Tasks/framework.rb
+++
@@ -1,6 +0,0 @@
-namespace :framework do
-  task :update_version do
-    version = File.read(File.join(FRAMEWORK.base_dir, 'FRAMEWORK_VERSION')).strip
-    FRAMEWORK.update_version(version)
-  end
-end
